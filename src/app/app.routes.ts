0e4cf0831995cf1f3e32f4ebd3bfdb8081dba548
Daniel24356 <igwedaniel128@gmail.com>
2025-04-14 07:21:11 -0700

    banking app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,20 +12,17 @@ export const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-    {
-        path: '',
-        redirectTo: 'dashboard/home',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-        children: [
-          { path: 'home', component: RightSideComponent },
-          { path: 'bank', component: BankComponent },
-          { path: 'transaction', component: TransactionComponent },
-          { path: 'payment', component: PaymentComponent }
-          // Add more child routes here
-        ]
-      }
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: RightSideComponent },
+      { path: 'bank', component: BankComponent },
+      { path: 'transaction', component: TransactionComponent },
+      { path: 'payment', component: PaymentComponent }
+    ]
+  },
+  // fallback route if user enters unknown URL
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
