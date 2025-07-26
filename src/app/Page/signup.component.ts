07819a5a8edb59372a5a200d05560f7e492d93bb
Daniel24356 <igwedaniel128@gmail.com>
2025-07-26 08:48:04 -0700

    routed the signUp and login

--- src/app/Page/signup.component.ts
+++ src/app/Page/signup.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
+  imports: [RouterModule],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
