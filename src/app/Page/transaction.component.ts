28244896acb987735a5aefb93a9d43a80e3828f8
Daniel24356 <igwedaniel128@gmail.com>
2025-07-27 06:41:00 -0700

    added onTransfer in transaction component

--- src/app/Page/transaction.component.ts
+++ src/app/Page/transaction.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './transaction.component.html',
   styleUrls: ['./transaction.component.scss']
 })
 export class TransactionComponent {
-  // You can add logic here later
+  transferData = {
+    fromAccount: '',
+    toAccount: '',
+    amount: ''
+  };
+
+ constructor(private http: HttpClient) {}
+ 
+  onTransfer() {
+    this.http.post('/fund-transfers', this.transferData).subscribe({
+      next: (res) => console.log('Transfer successful', res),
+      error: (err) => console.error('Transfer failed', err)
+    });
+  }
+
 }
