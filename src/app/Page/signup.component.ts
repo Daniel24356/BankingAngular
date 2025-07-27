cfbb9591e13db49c72a4c61b021d305f8129dd68
Daniel24356 <igwedaniel128@gmail.com>
2025-07-27 06:09:53 -0700

    consumed the register api

--- src/app/Page/signup.component.ts
+++ src/app/Page/signup.component.ts
@@ -1,19 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterModule],
+   standalone: true,
+  imports: [RouterModule,CommonModule, FormsModule],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
-  name: string = '';
-  email: string = '';
-  password: string = '';
+    user = {
+    firstName: '',
+    lastName: '',
+    emailId: '',
+    contactNumber: '',
+    password: ''
+  };
 
-  onSignup() {
-    // Implement your signup logic here
-    console.log('Signing up with:', this.name, this.email, this.password);
+  constructor(private http: HttpClient) {}
+
+   onRegister() {
+    this.http.post('/api/users/register', this.user).subscribe({
+      next: res => console.log('Registration successful', res),
+      error: err => console.error('Error registering', err)
+    });
   }
 }
