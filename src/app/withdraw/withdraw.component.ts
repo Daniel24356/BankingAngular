dc4904e0e2dd98899d66435c9e4688d636831a9d
Daniel24356 <igwedaniel128@gmail.com>
2025-07-27 08:29:21 -0700

    created withdraw component

---
+++ src/app/withdraw/withdraw.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-withdraw',
+  imports: [],
+  templateUrl: './withdraw.component.html',
+  styleUrl: './withdraw.component.scss'
+})
+export class WithdrawComponent {
+
+}
