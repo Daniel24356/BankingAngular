0a8bf238aa258d30e54e765805dff3832c61554a
Daniel24356 <igwedaniel128@gmail.com>
2025-07-27 08:31:57 -0700

    designed withdrawal component

--- src/app/withdraw/withdraw.component.ts
+++ src/app/withdraw/withdraw.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-withdraw',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './withdraw.component.html',
   styleUrl: './withdraw.component.scss'
 })
 export class WithdrawComponent {
+  formData = {
+    accountId: '',
+    transactionType: 'WITHDRAWAL',
+    amount: '',
+    description: '',
+  };
 
+   message = '';
+
+  constructor(private http: HttpClient) {}
+
+  
 }
