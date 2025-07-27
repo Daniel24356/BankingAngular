6f0ac6cef85ae5baf5541a3bf57ef60c489b9aea
Daniel24356 <igwedaniel128@gmail.com>
2025-07-27 07:21:41 -0700

    updated create user componet

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, CommonModule, FormsModule],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.scss'
 })
 export class CreateUserComponent {
+   formData = {
+    accountType: '',
+    userId: ''
+  };
+
+   message = '';
+  loading = false;
+
+   constructor(private http: HttpClient) {}
+
+    createAccount() {
+    if (!this.formData.accountType || !this.formData.userId) {
+      this.message = 'All fields are required.';
+      return;
+    }
+
+    this.loading = true;
+    this.http.post('/accounts', this.formData).subscribe({
+      next: (res: any) => {
+        this.message = 'Account successfully created!';
+        this.loading = false;
+      },
+      error: (err) => {
+        this.message = 'Failed to create account.';
+        console.error(err);
+        this.loading = false;
+      }
+    });
+  }
 
 }
