72c4c799b61b191a87a5800fb20fabf1a62686cb
Daniel24356 <igwedaniel128@gmail.com>
2025-07-29 03:18:13 -0700

    designed admin transaction component

--- src/app/admin-transaction/admin-transaction.component.ts
+++ src/app/admin-transaction/admin-transaction.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-admin-transaction',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './admin-transaction.component.html',
   styleUrl: './admin-transaction.component.scss'
 })
 export class AdminTransactionComponent {
-
+  
 }
