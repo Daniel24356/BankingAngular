7036c026e43979610195f0d4451b5eb4a5934b64
Daniel24356 <igwedaniel128@gmail.com>
2025-07-29 02:54:35 -0700

    created admin transaction

---
+++ src/app/admin-transaction/admin-transaction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-transaction',
+  imports: [],
+  templateUrl: './admin-transaction.component.html',
+  styleUrl: './admin-transaction.component.scss'
+})
+export class AdminTransactionComponent {
+
+}
